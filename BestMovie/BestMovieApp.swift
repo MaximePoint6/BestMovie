96e241fa6ac9e6746eaf5c9268bf81f20eb57acb
Maxime Point <maximepoint6@gmail.com>
2022-03-26 17:19:11 +0100

    Initial Commit

---
+++ BestMovie/BestMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestMovieApp.swift
+//  BestMovie
+//
+//  Created by Maxime Point on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BestMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
